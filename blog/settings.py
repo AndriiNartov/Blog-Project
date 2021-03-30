fb4c220a7d1007aae95d530f55e7b24b9ef35c8e
AndriiNartov <80200103+AndriiNartov@users.noreply.github.com>
2021-03-30 10:59:11 +0200

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'postgres',
-        'PASSWORD': 'kirajana1709',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
